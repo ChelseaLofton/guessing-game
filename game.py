f318e144760153ac532ab63ae918b5aa8fd34434
Chelsea Lofton <chelsealofton77@gmail.com>
2023-01-23 18:31:19 -0800

    latest updates

--- game.py
+++ game.py
@@ -1,17 +1,32 @@
 import random
 
-secret_number = random.randint(1, 101)
-print("Hello! What is your name?")
-name = input("> ")
-print(f"Hello {name}!")
+def guess_number():
+    secret_number = random.randint(1, 101)
+    print("Hello! What is your name?")
+    name = input("> ")
+    print(f"Hello {name}!")
+    num_guesses = 0
 
-while True:
-    print("please choose a number between 1 and 100")
-    guess = int(input("> "))
-    if guess > secret_number:
-        print("Too high!")
-    elif guess < secret_number:
-        print("Too low!")
-    else:
-        print(f"Congrats {name}! You guessed the number!")
-        break
+    while True:
+        print("please choose a number between 1 and 100")
+        guess = input("> ")
+        if guess.isnumeric():
+            guess = int(guess)
+            if guess in range(1, 100):
+                if guess > secret_number:
+                    print("Too high!")
+                    num_guesses += 1
+                    print(num_guesses)
+                elif guess < secret_number:
+                    print("Too low!")
+                    num_guesses += 1
+                    print(num_guesses)
+                else:
+                    print(f"Congrats {name}! You guessed the number! You got it in ", (num_guesses+1), " guesses.")
+                    break
+            else:
+                print("ERROR: Please only type a number between 1 and 100")
+        else:
+            print("ERROR: Please only type a number between 1 and 100")
+
+guess_number()
